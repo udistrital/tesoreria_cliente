2abf8e70dbe6b8f5f4c98a6d0eb11775569d06af
Anamaria Miranda <miranda_anam@hotmail.com>
2021-03-25 15:25:37 -0500

    feat: se cambia el token para acceder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -47,7 +47,7 @@ export const environment = {
     },
     TOKEN: {
         AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
-        CLIENTE_ID: 'xsCo1iC2gxLYFl8RVEZqZl4ST2oa',
+        CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
         RESPONSE_TYPE: 'id_token token',
         SCOPE: 'openid email role',
         REDIRECT_URL: 'http://localhost:4200/',
