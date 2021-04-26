4615ceee783fa35b04adc7720b945152ddef84cc
Anamaria <miranda_anam@hotmail.com>
2021-04-26 15:32:45 -0500

    se agrega vista de consignaciones #99

--- src/app/pages/ingresos/components/set-consignaciones/set-consignaciones.component.ts
+++ src/app/pages/ingresos/components/set-consignaciones/set-consignaciones.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { SharedService } from '../../../../shared/services/shared.service';
+import { getTipoIngreso } from '../../selectors/ingresos.selectors';
+import { Router } from '@angular/router';
+import { CONF_CONSIGNACION, CONF_CODIGO_BARRAS, CONF_APORTES, DATOS_ICETEX, DATOS_PSE, DATOS_ACH, DATOS_RECAUDO, DATOS_APORTES_NACION, DATOS_APORTES_DISTRITO, DATOS_CODIGO_BARRAS } from '../../interfaces/interfaces';
+import { DATOS_ENTIDADES } from '../../../giros/seguridadsocial/interfaces/interfaces';
 
 @Component({
   selector: 'ngx-set-consignaciones',
@@ -7,9 +13,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetConsignacionesComponent implements OnInit {
 
-  constructor() { }
+  configurationConsignaciones: any;
+  datosConsignaciones: any;
+  tipoIngreso: any;
+
+  constructor(
+    private store: Store<any>,
+    private sharedService: SharedService,
+    private route: Router,
+  ) { }
 
   ngOnInit() {
+    this.store.select(getTipoIngreso).subscribe(
+      data => {
+        if (this.sharedService.IfStore(data)) {
+          switch (data.tipo) {
+            case 'icetex':
+              this.configurationConsignaciones = CONF_CONSIGNACION;
+              this.datosConsignaciones = DATOS_ICETEX;
+              break;
+            case 'barras':
+              this.configurationConsignaciones = CONF_CODIGO_BARRAS;
+              this.datosConsignaciones = DATOS_CODIGO_BARRAS;
+              break;
+            case 'pse':
+              this.configurationConsignaciones = CONF_CONSIGNACION;
+              this.datosConsignaciones = DATOS_PSE;
+              break;
+            case 'ach':
+              this.configurationConsignaciones = CONF_CONSIGNACION;
+              this.datosConsignaciones = DATOS_ACH;
+              break;
+            case 'recaudoLinea':
+              this.configurationConsignaciones = CONF_CONSIGNACION;
+              this.datosConsignaciones = DATOS_RECAUDO;
+              break;
+            case 'aportesNacion':
+              this.configurationConsignaciones = CONF_APORTES;
+              this.datosConsignaciones = DATOS_APORTES_NACION;
+              break;
+            case 'aportesDistrito':
+              this.configurationConsignaciones = CONF_APORTES;
+              this.datosConsignaciones = DATOS_APORTES_DISTRITO;
+              break;
+            case 'otrasEntidades':
+              this.configurationConsignaciones = CONF_CONSIGNACION;
+              this.datosConsignaciones = DATOS_ENTIDADES;
+              break;
+            default:
+              this.regresar();
+              break;
+          }
+          this.tipoIngreso = data.tipo;
+        }
+      }
+    );
   }
+
+  total() {
+  }
+
+  totalEfectivo() {
+
+  }
+
+  regresar() {
+    this.route.navigateByUrl('pages/ingresos/' + this.tipoIngreso + '/lista');
+  }
+
+  totalCheque() {}
+
+  totalDatafono() {}
 
 }
--- src/app/pages/ingresos/interfaces/interfaces.ts
+++ src/app/pages/ingresos/interfaces/interfaces.ts
@@ -224,8 +224,12 @@ export const CONF_CONSIGNACION: any = {
             }
         },
     ],
-    sort: false,
-    filter: false,
+    noData: {
+        name: 'No existen elementos asociados',
+        class: 'text-center',
+    },
+    sort: true,
+    filter: true,
 };
 export const CONF_RUBROS: any = {
     showColumnTitle: true,
@@ -352,8 +356,12 @@ export const CONF_CONTABILIDAD: any = {
             }
         }
     ],
-    sort: false,
-    filter: false,
+    noData: {
+        name: 'No existen elementos asociados',
+        class: 'text-center',
+    },
+    sort: true,
+    filter: true,
 };
 export const CONF_CODIGO_BARRAS: any = {
     showColumnTitle: true,
@@ -491,10 +499,14 @@ export const CONF_CODIGO_BARRAS: any = {
             }
         },
     ],
+    noData: {
+        name: 'No existen elementos asociados',
+        class: 'text-center',
+    },
     sort: true,
-    filter: false,
+    filter: true,
 };
-export const CONF_DESCUENTOS: any = {
+export const CONF_APORTES: any = {
     showColumnTitle: true,
     // title: {
     //     name: 'Actividades Asociadas',
@@ -595,10 +607,10 @@ export const CONF_DESCUENTOS: any = {
         }
     ],
     sort: true,
-    filter: false,
+    filter: true,
 };
  /******  Datos dummy ******************/
- export const DATOS_INGRESOS: any = [
+export const DATOS_INGRESOS: any = [
     {
         vigencia: 2020,
         consecutivo: 32,
@@ -614,5 +626,172 @@ export const CONF_DESCUENTOS: any = {
         areaFuncional: 1,
         valorTotal: 6523421,
         fecha: '2021/01/01',
+    }
+];
+export const DATOS_ICETEX: any = [
+    {
+        oficina: 519,
+        ciudad: 'Bogotá',
+        fechaRecaudo: '2020/06/01',
+        depositante: 'ICETEX',
+        referencia1: 8127361,
+        referencia2: 12391283,
+        valorEfectivo: 0,
+        valorCheque: 2345678,
+        valorDatafono: 0,
+        valorTotal: 2345678,
+        otrasReferencias: 9672,
+    },
+    {
+        oficina: 519,
+        ciudad: 'Bogotá',
+        fechaRecaudo: '2020/06/02',
+        depositante: 'ICETEX',
+        referencia1: 8723481,
+        referencia2: 10893298,
+        valorEfectivo: 0,
+        valorCheque: 3467894,
+        valorDatafono: 0,
+        valorTotal: 3467894,
+        otrasReferencias: 9672,
+    }
+];
+export const DATOS_PSE: any = [
+    {
+        oficina: '',
+        ciudad: 'Bogotá',
+        fechaRecaudo: '2020/05/02',
+        depositante: 'Andrea Arias',
+        referencia1: 823422,
+        referencia2: 1857723,
+        valorEfectivo: 0,
+        valorCheque: 0,
+        valorDatafono: 3467894,
+        valorTotal: 3467894,
+        otrasReferencias: 9672,
+    },
+    {
+        oficina: '',
+        ciudad: 'Bogotá',
+        fechaRecaudo: '2020/06/10',
+        depositante: 'Fernando Duran',
+        referencia1: 8723481,
+        referencia2: 10893298,
+        valorEfectivo: 0,
+        valorCheque: 0,
+        valorDatafono: 7735983,
+        valorTotal: 7735983,
+        otrasReferencias: 9672,
+    }
+];
+export const DATOS_ACH: any = [
+    {
+        oficina: '',
+        ciudad: 'Bogotá',
+        fechaRecaudo: '2020/06/05',
+        depositante: 'Banco de Bogotá',
+        referencia1: 8943052,
+        referencia2: 1823405,
+        valorEfectivo: 0,
+        valorCheque: 0,
+        valorDatafono: 1238890,
+        valorTotal: 0,
+        otrasReferencias: 9672,
+    },
+    {
+        oficina: '',
+        ciudad: 'Bogotá',
+        fechaRecaudo: '2020/06/12',
+        depositante: 'BBVA',
+        referencia1: 792347,
+        referencia2: 1023948,
+        valorEfectivo: 0,
+        valorCheque: 0,
+        valorDatafono: 5938048,
+        valorTotal: 5938048,
+        otrasReferencias: 9672,
+    }
+];
+export const DATOS_RECAUDO: any = [
+    {
+        oficina: 519,
+        ciudad: 'Bogotá',
+        fechaRecaudo: '2020/06/05',
+        depositante: 'Richard Buitrago',
+        referencia1: 10327409,
+        referencia2: 10546478,
+        valorEfectivo: 14000,
+        valorCheque: 0,
+        valorDatafono: 0,
+        valorTotal: 14000,
+        otrasReferencias: 675,
+    },
+    {
+        oficina: 519,
+        ciudad: 'Bogotá',
+        fechaRecaudo: '2020/06/07',
+        depositante: 'Lina Luis',
+        referencia1: 2180123,
+        referencia2: 2384081,
+        valorEfectivo: 70000,
+        valorCheque: 0,
+        valorDatafono: 0,
+        valorTotal: 70000,
+        otrasReferencias: 98,
+    }
+];
+export const DATOS_APORTES_NACION: any = [
+    {
+        fechaConsignacion: '2019/08/01',
+        entidadFinanciera: 'Banco de occidente',
+        numeroCuenta: 230141209,
+        tipoCuenta: 'Ahorros',
+        vigencia: 2018,
+        valor: 28345779,
+        tipoDocumento: 'Recibo de caja',
+        numeroDocumento: 'Res No. 007',
+        fechaDocumento: '2019/07/26',
+    }
+];
+export const DATOS_APORTES_DISTRITO: any = [
+    {
+        fechaConsignacion: '2019/08/03',
+        entidadFinanciera: 'BBVA',
+        numeroCuenta: 3294021,
+        tipoCuenta: 'Corriente',
+        vigencia: 2019,
+        valor: 23842934,
+        tipoDocumento: 'Recibo de caja',
+        numeroDocumento: 'Res No. 012',
+        fechaDocumento: '2019/07/24',
+    }
+];
+export const DATOS_CODIGO_BARRAS: any = [
+    {
+        fechaConsignacion: '2019/08/03',
+        entidadFinanciera: 'BBVA',
+        numeroCuenta: 3294021,
+        tipoCuenta: 'Corriente',
+        vigencia: 2019,
+        valor: 23842934,
+        tipoDocumento: 'Recibo de caja',
+        numeroDocumento: 'Res No. 012',
+        fechaDocumento: '2019/07/24',
+    }
+];
+
+export const DATOS_OTRAS_ENTIDADES: any = [
+    {
+        oficina: '',
+        ciudad: 'Bogotá',
+        fechaRecaudo: '2020/06/07',
+        depositante: '',
+        referencia1: 423487,
+        referencia2: 892347,
+        valorEfectivo: 0,
+        valorCheque: 0,
+        valorDatafono: 5800234,
+        valorTotal: 5800234,
+        otrasReferencias: 324,
     }
 ];
